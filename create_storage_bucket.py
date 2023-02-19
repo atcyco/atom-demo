3ba1eb27b06c58d00101d7048d770415462f18bf
atcyco <125686038+atcyco@users.noreply.github.com>
2023-02-19 15:01:05 -0300

    starting to create cloud storage

---
+++ create_storage_bucket.py
@@ -0,0 +1,13 @@
+import os
+from google.cloud import storage
+
+# Set the `GOOGLE_APPLICATION_CREDENTIALS` environment variable
+# to the path of your service account key file
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'gcp_key/atom-demo-378221-0b3966105752.json'
+
+# Create a client object
+client = storage.Client()
+
+# Create a new bucket with a unique name
+bucket_name = 'my-test-bucket-with-python'
+bucket = client.create_bucket(bucket_name)
